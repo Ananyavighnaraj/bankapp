964c80032df0b6e35f264a78b309e8bf056a4805
unknown <ananyavighnaraj@gmail.com>
2022-12-06 20:21:19 +0530

    2nd commit

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -18,30 +18,7 @@ aim="YOUR PERFECT BANKING PARTNER"
     1003:{acno:1003,username:"mega",password:123,balance:0}
   }
 
-// login(){     for method 1.2.2
-//   var acno=this.acno
-//   var psw=this.psw
-//   var userDetails=this.userDetails
-
-//   if(acno in userDetails){
-//     if(psw==userDetails[acno]["password"]){
-//     alert('login success')
-//   }
-//   else{
-//     alert('incorrect password')
-//   }
-// }
-//   else{
-//     alert('incorrect username')
-//   }
-//  // alert('login clicked')
-// }
-
-
-login(a:any,b:any){     //for method 1.2.3   provide any name for arguments as that is passed on html 
-  this.acno=a.value
-  this.psw=b.value    //these 2 lines to call a and b for 1.2.3
-
+login(){   //  for method 1.2.2
   var acno=this.acno
   var psw=this.psw
   var userDetails=this.userDetails
@@ -57,8 +34,31 @@ login(a:any,b:any){     //for method 1.2.3   provide any name for arguments as t
   else{
     alert('incorrect username')
   }
+ // alert('login clicked')
 }
 }
+
+// login(a:any,b:any){     //for method 1.2.3   provide any name for arguments as that is passed on html 
+//   this.acno=a.value
+//   this.psw=b.value    //these 2 lines to call a and b for 1.2.3  acno and psw are initialised at start
+
+//   var acno=this.acno
+//   var psw=this.psw
+//   var userDetails=this.userDetails
+
+//   if(acno in userDetails){
+//     if(psw==userDetails[acno]["password"]){
+//     alert('login success')
+//   }
+//   else{
+//     alert('incorrect password')
+//   }
+// }
+//   else{
+//     alert('incorrect username')
+//   }
+// }
+// }
 
 // acnoChange(event:any){    //for $event
 // this.acno=event.target.value
